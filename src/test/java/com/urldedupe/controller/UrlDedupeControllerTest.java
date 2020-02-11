87bee47d7b8d1d64811889bd296f230a135b0d4b
Maddison Case <mcase@thrivehive.com>
2020-02-11 13:28:57 -0700

    Couple more tests

--- src/test/java/com/urldedupe/controller/UrlDedupeControllerTest.java
+++ src/test/java/com/urldedupe/controller/UrlDedupeControllerTest.java
@@ -3,15 +3,12 @@ package com.urldedupe.controller;
 import com.urldedupe.domain.service.UrlService;
 import com.urldedupe.service.UrlFileReader;
 import org.assertj.core.util.Sets;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.web.servlet.MockMvc;
 
 import java.io.File;
 import java.util.Arrays;
@@ -23,74 +20,63 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-class UrlDedupeControllerTest {
+public class UrlDedupeControllerTest {
 
     UrlDedupeController controller;
-
-    @Autowired
-    private MockMvc mockMvc;
-
     @Mock
     private UrlService mockUrlService;
-
     @Mock
     private UrlFileReader mockUrlFileReader;
 
-    @BeforeEach
-    void setUp() {
+    private static final String DIR_NAME = "testUrls";
+    private static final String TEST_FILE_DIR = "./src/test/java/testFiles";
+
+    @Before
+    public void setUp() {
         MockitoAnnotations.initMocks(this);
         controller = new UrlDedupeController(mockUrlService, mockUrlFileReader);
     }
 
-    @Nested
-    class Create {
-        private static final String DIR_NAME = "testUrls";
-        private static final String ENDPOINT = "/urldedupe/" + DIR_NAME;
-        private static final String TEST_FILE_DIR = "./src/test/java/testFiles";
-
-        @AfterEach
-        void tearDown() {
+    @After
+    public void tearDown() {
             Mockito.clearInvocations(mockUrlService, mockUrlFileReader);
         }
 
+    @Test
+    public void success() throws Exception {
+        List<File> testFiles = createTestFiles();
+        Set<String> testOneSetValues = Sets.newHashSet();
+        testOneSetValues.add("testOneSetUrl");
+        Set<String> testTwoSetValues = Sets.newHashSet();
+        testTwoSetValues.add("testTwoSetUrl");
+        Set<String> testThreeSetValues = Sets.newHashSet();
 
-        @Test
-        void success() throws Exception {
-            List<File> testFiles = createTestFiles();
-            Set<String> testOneSetValues = Sets.newHashSet();
-            testOneSetValues.add("testOneSetUrl");
-            Set<String> testTwoSetValues = Sets.newHashSet();
-            testTwoSetValues.add("testTwoSetUrl");
-            Set<String> testThreeSetValues = Sets.newHashSet();
-
-            for (File file : testFiles) {
-                if ("TestOne".equals(file.getName())) {
-                    doReturn(testOneSetValues).when(mockUrlFileReader).getUrls(file);
-                } else if ("TestTwo".equals(file.getName())) {
-                    doReturn(testTwoSetValues).when(mockUrlFileReader).getUrls(file);
-                } else if ("TestThree".equals(file.getName())) {
-                    doReturn(testThreeSetValues).when(mockUrlFileReader).getUrls(file);
-                }
+        for (File file : testFiles) {
+            if ("TestOne".equals(file.getName())) {
+                doReturn(testOneSetValues).when(mockUrlFileReader).getUrls(file);
+            } else if ("TestTwo".equals(file.getName())) {
+                doReturn(testTwoSetValues).when(mockUrlFileReader).getUrls(file);
+            } else if ("TestThree".equals(file.getName())) {
+                doReturn(testThreeSetValues).when(mockUrlFileReader).getUrls(file);
             }
-
-            doReturn(testFiles).when(mockUrlFileReader.getFiles(DIR_NAME));
-            mockMvc.perform(get(ENDPOINT)).andExpect(status().isOk());
-
-            verify(mockUrlService, times(3)).saveAll(any());
-            verify(mockUrlService, times(1)).saveAll(testOneSetValues);
-            verify(mockUrlService, times(1)).saveAll(testTwoSetValues);
-            verify(mockUrlService, times(1)).saveAll(testThreeSetValues);
         }
 
-        private List<File> createTestFiles() {
-            File dir = new File(TEST_FILE_DIR);
-            File[] files = dir.listFiles();
-            return files != null ?
-                   Arrays.asList(files) :
-                   Collections.emptyList();
-        }
+        doReturn(testFiles).when(mockUrlFileReader).getFiles(DIR_NAME);
+
+        controller.create(DIR_NAME);
+
+        verify(mockUrlService, times(3)).saveAll(any());
+        verify(mockUrlService, times(1)).saveAll(testOneSetValues);
+        verify(mockUrlService, times(1)).saveAll(testTwoSetValues);
+        verify(mockUrlService, times(1)).saveAll(testThreeSetValues);
+    }
+
+    private List<File> createTestFiles() {
+        File dir = new File(TEST_FILE_DIR);
+        File[] files = dir.listFiles();
+        return files != null ?
+               Arrays.asList(files) :
+               Collections.emptyList();
     }
 }
--- src/test/java/com/urldedupe/domain/service/impl/UrlServiceInMemoryImplTest.java
+++ src/test/java/com/urldedupe/domain/service/impl/UrlServiceInMemoryImplTest.java
@@ -3,9 +3,9 @@ package com.urldedupe.domain.service.impl;
 import com.urldedupe.domain.Url;
 import com.urldedupe.domain.repository.UrlRepository;
 import org.assertj.core.util.Sets;
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -15,59 +15,59 @@ import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
-class UrlServiceInMemoryImplTest {
+public class UrlServiceInMemoryImplTest {
 
     UrlServiceInMemoryImpl service;
 
     @Mock
     private UrlRepository mockUrlRepository;
 
-    @BeforeEach
-    void setUp() {
+    @Before
+    public void setUp() {
         MockitoAnnotations.initMocks(this);
         service = new UrlServiceInMemoryImpl(mockUrlRepository);
     }
 
-    @Nested
-    class SaveAll {
 
-        private String url1 = "http://www.url1.com";
-        private String url2 = "http://www.url2.com";
-        private String url3 = "http://www.url3.com";
+    private String url1 = "http://www.url1.com";
+    private String url2 = "http://www.url2.com";
+    private String url3 = "http://www.url3.com";
 
-        @Test
-        void success() {
-            Set<String> urls = Sets.newHashSet();
-            urls.add(url1);
-            urls.add(url2);
-            urls.add(url3);
-            doThrow(new DataIntegrityViolationException("boom!")).when(mockUrlRepository).save(createUrl(url1));
-            doThrow(new RuntimeException("boom!")).when(mockUrlRepository).save(createUrl(url2));
+    @Test
+    public void success() {
 
-            verify(mockUrlRepository, times(1)).save(createUrl(url1));
-            verify(mockUrlRepository, times(1)).save(createUrl(url2));
-            verify(mockUrlRepository, times(1)).save(createUrl(url3));
-        }
+        Set<String> urls = Sets.newHashSet();
+        urls.add(url1);
+        urls.add(url2);
+        urls.add(url3);
+        doThrow(new RuntimeException()).when(mockUrlRepository).save(createUrl(url2));
 
-        @Test
-        void emptySet() {
-            service.saveAll(Collections.emptySet());
-            verify(mockUrlRepository, times(0)).save(any());
-        }
+        service.saveAll(urls);
 
-        @Test
-        void npe() {
+        verify(mockUrlRepository, times(1)).save(createUrl(url1));
+        verify(mockUrlRepository, times(1)).save(createUrl(url2));
+        verify(mockUrlRepository, times(1)).save(createUrl(url3));
+    }
+
+    @Test
+    public void emptySet() {
+        service.saveAll(Collections.emptySet());
+        verify(mockUrlRepository, times(0)).save(any());
+    }
+
+    @Test
+    public void npe() {
             assertThrows(NullPointerException.class, () -> service.saveAll(null));
         }
 
-        private Url createUrl(final String url) {
-            return Url.builder()
-                      .url(url)
-                      .build();
-        }
+    private Url createUrl(final String url) {
+        return Url.builder()
+                  .url(url)
+                  .build();
     }
 }
